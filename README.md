1bb99a9c7dfe3cb460aed8a4fbf306054d8ee50b
rkarhila <reima.karhila@aalto.fi>
2017-02-23 14:58:28 +0200

    initial code commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## A simple graph-based sorting demo ##
+
+Before doing anything, install sortable with
+
+`npm install sortablejs --save`
---
+++ js/sorter.js
@@ -0,0 +1,7 @@
+
+var el1 = document.getElementById('ok_items');
+var sortable = Sortable.create(el1, {group: "all"});
+
+
+var el2 = document.getElementById('bad_items');
+var sortable = Sortable.create(el2, {group: "all"});
