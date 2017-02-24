3cad6ced923d54c3e17936bde01becc11ee466f1
rkarhila <reima.karhila@aalto.fi>
2017-02-24 11:43:28 +0200

    Some real text samples included

--- js/sorter.js
+++ js/sorter.js
@@ -1,81 +1,67 @@
 
-var samples=[
-    {audio: "01.wav", text: "Tvättas 1"},
-    {audio: "02.wav", text: "Tvättas 2"},
-    {audio: "03.wav", text: "Tvättas 3"},
-    {audio: "04.wav", text: "Tvättas 4"},
-    {audio: "05.wav", text: "Tvättas 5"},    
-    {audio: "06.wav", text: "Tvättas 6"},
-    {audio: "07.wav", text: "Tvättas 7"},
-    {audio: "08.wav", text: "Tvättas 8"},
-    {audio: "09.wav", text: "Tvättas 9"},
-    {audio: "10.wav", text: "Tvättas 10"},    
-    {audio: "11.wav", text: "Tvättas 11"},
-    {audio: "12.wav", text: "Tvättas 12"},
-    {audio: "13.wav", text: "Tvättas 13"},
-    {audio: "14.wav", text: "Tvättas 14"},
-    {audio: "15.wav", text: "Tvättas 15"},    
-]
 
+var updateAllSamplesList = function () {
 
-var sample_container = document.getElementById('allsamples');
+    var sample_container = document.getElementById('allsamples');
+    
+    while (sample_container.firstChild) {
+	    sample_container.removeChild(sample_container.firstChild);
+    }
+    
+    samples.forEach(function(sample) {
+	new_elem = document.createElement("div");
+	new_elem.className = "samplediv";
+	
+	new_content = document.createElement("span");
+	//new_content.className = "sample";
+	new_content.innerHTML = sample.text;
+	
+	new_content2 = document.createElement("span");
+	new_content2.className = "samplescore";
+	new_content2.id = sample.id;
+	new_content2.innerHTML = sample.score;
+	
+	new_elem.appendChild(new_content);
+	new_elem.appendChild(new_content2);
+	
+	sample_container.appendChild(new_elem);
+    });
+}
 
-samples.forEach(function(sample) {
-    new_elem = document.createElement("div");
-    new_elem.className = "samplediv";
-    new_content = document.createElement("span");
-    new_content.className = "sample";
-    new_content.innerHTML = sample.text;
-    new_elem.appendChild(new_content);
-    sample_container.appendChild(new_elem);
-});
+updateAllSamplesList();
 
-shuffle(samples)
 
 var addToList = function(sample, list) {
     new_elem = document.createElement("li");
     new_elem.className = "sample";
+    new_elem.id = sample.id;
     new_elem.innerHTML = sample.text;
     list.appendChild(new_elem);
 }
 
 
 var list1 = document.getElementById('phonetics_list1');
-var list2 = document.getElementById('phonetics_list2');
-var list3 = document.getElementById('phonetics_list3');
+var sample_index=[ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 ];
+var sliceindex = 0;
+var round = 0;
+var test = 0;
 
-samples.slice(0,5).forEach( function(sample) {
-    addToList(sample, list1 );
-});
-samples.slice(5,10).forEach( function(sample) {
-    addToList(sample, list2 );
-});
-samples.slice(10,15).forEach( function(sample) {
-    addToList(sample, list3 );
-});
+var updateSortingList = function () {
+    if (sliceindex + 5 < samples.length) {
+	shuffle(sample_index);
+	sliceindex = 0;
+	round += 1;	
+	document.getElementById('round').innerHTML=round;
+    }
+    test += 1;
+    sample_index.slice(sliceindex,sliceindex+5).forEach( function(index) {
+	addToList(samples[index], list1 );
+    });
 
-shuffle(samples)
+    document.getElementById('test').innerHTML=test;
+}
 
-var list4 = document.getElementById('content_list1');
-var list5 = document.getElementById('content_list2');
-var list6 = document.getElementById('content_list3');
+updateSortingList()
 
-samples.slice(0,5).forEach( function(sample) {
-    addToList(sample, list4 );
-});
-samples.slice(5,10).forEach( function(sample) {
-    addToList(sample, list5 );
-});
-samples.slice(10,15).forEach( function(sample) {
-    addToList(sample, list6 );
-});
-
-
-var sortable1 = Sortable.create(list1, {onUpdate: updateOrder});
-var sortable2 = Sortable.create(list2, {onUpdate: updateOrder});
-var sortable3 = Sortable.create(list3, {onUpdate: updateOrder});
-var sortable4 = Sortable.create(list4, {onUpdate: updateOrder});
-var sortable5 = Sortable.create(list5, {onUpdate: updateOrder});
-var sortable6 = Sortable.create(list6, {onUpdate: updateOrder});
-
+var sortable1 = Sortable.create(list1);
 lists_ready=true;
--- js/updateorder.js
+++ js/updateorder.js
@@ -1,37 +1,63 @@
 
-var updateOrder = function(evt1, evt2) {
+var updateOrder = function(samples) {
     if (lists_ready) {
 	console.log("Lists ready and function called!");
 
 	var list1 = document.getElementById('phonetics_list1');
-	var list2 = document.getElementById('phonetics_list2');
-	var list3 = document.getElementById('phonetics_list3');
+	lists = [ list1 ];
 
-	var list4 = document.getElementById('content_list1');
-	var list5 = document.getElementById('content_list2');
-	var list6 = document.getElementById('content_list3');
-
-	var lists = [ list1, list2, list3, list4, list5, list6 ];
+	score_to_give=2;
 
 	var grand_list = document.getElementById('finalorderlist');
+
+	while (list1.firstChild) {
+	    if (list1.firstChild.className = "sample") {
+		console.log("First child:");
+		console.log(list1.firstChild);
+		console.log(list1.firstChild.id);
+		if (typeof(list1.firstChild.id) != 'undefined') {
+		    samples[list1.firstChild.id].score += score_to_give;
+		    score_to_give -= 1;
+		}
+	    }
+	    list1.removeChild(list1.firstChild);	    
+	}
+	   
+	updateAllSamplesList();
+
 	while (grand_list.firstChild) {
 	    grand_list.removeChild(grand_list.firstChild);
+	    
 	}
 
-	var addToList = function(sample, list) {
-	    console.log(sample);
-	    new_elem = document.createElement("li");
-	    new_elem.className = "ordered_sample";
-	    new_elem.innerHTML = sample.innerHTML;
+	var addToList2 = function(index, list) {
+	    console.log(samples);
+	    console.log(index);
+	    sample = samples[index[0]];
+	    //console.log(sample);
+
+	    new_elem = document.createElement("div");
+	    new_elem.className = "samplediv";
+	    new_elem.innerHTML = sample.text;
 	    list.appendChild(new_elem);
+	    list.insertBefore(new_elem, list.firstChild);
 	}
 
-	lists.forEach(function(list) {
-	    list.childNodes.forEach( function(sample, position) {
-		if (sample.className == "sample")
-		    addToList(sample, grand_list);
-	    });
+	sortable=[];
+	
+	samples.forEach(function(sample) {
+	    //console.log(sample)
+	    sortable.push([sample.id, sample.score])
 	});
+	sortable.sort(function(a, b) {
+	    return a[1] - b[1]
+	})
+
+	sortable.forEach(function(index) {
+	    addToList2(index, grand_list);
+	});
+
+	updateSortingList();
 
     }
     else
