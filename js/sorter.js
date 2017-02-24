8c405aad24b0ea6aaef6349af2ab81fa338bb44f
rkarhila <reima.karhila@aalto.fi>
2017-02-24 13:01:16 +0200

    Persistent garbage class (cant remove from garbage though)

--- js/sorter.js
+++ js/sorter.js
@@ -49,38 +49,40 @@ var list1 = document.getElementById('phonetics_list1');
 var rejected_list1 = document.getElementById('rejected_list1');
 
 var sample_index=[ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 ];
+shuffle(sample_index);
+
 var sliceindex = 0;
 var round = 0;
 var test = 0;
 
-var updateSortingList = function () {
-    //if (sliceindex + 5 < samples.length) {
-    shuffle(sample_index);
-    //sliceindex = 0;
-    //round += 1;	
-    //document.getElementById('round').innerHTML=round;
-    //}
-    test += 1;
-    //sample_index.slice(sliceindex,sliceindex+5).forEach( function(index) {
-    //addToList(samples[index], list1 );
-    //});
-    index=0
-    listcounter=0;
-    while (listcounter < 5) {
-	if (samples[index].score > -100) {
-	    addToList(samples[index], list1 );
-	    listcounter++;
-	}
-	index++
-	if (index > samples.length)
-	    break;
-    }
+var sliceindex = 0;
 
+
+var updateSortingList = function () {
+    //console.log(sliceindex +" + 5 > "+ samples.length)
+    if (sliceindex + 5 > samples.length) {
+	console.log("shuffling!");
+	shuffle(sample_index);
+	sliceindex = 0;
+	round += 1;	
+	document.getElementById('round').innerHTML=round;
+    }
+    test += 1;
+    //console.log(sample_index.slice(sliceindex,sliceindex+5))
+    sample_index.slice(sliceindex,sliceindex+5).forEach( function(index) {
+	if (samples[ index ].score > -100) {
+	    addToList(samples[index], list1 );
+	}
+	else {
+	    addToList(samples[index], rejected_list1 );
+	}
+    });
+    sliceindex += 5;
     document.getElementById('test').innerHTML=test;
 }
 
 updateSortingList()
 
 var sortable1 = Sortable.create(list1, {group: "all"});
-var sortable2 = Sortable.create(rejected_list1, {group: "all"});
+var sortable2 = Sortable.create(rejected_list1, {group: { name: "all", put: true, pull: false}});
 lists_ready=true;
--- js/updateorder.js
+++ js/updateorder.js
@@ -6,29 +6,45 @@ var updateOrder = function(samples) {
 	var list1 = document.getElementById('phonetics_list1');
 	lists = [ list1 ];
 
-	score_to_give=2;
 
 	var grand_list = document.getElementById('finalorderlist');
 
-	while (list1.firstChild) {
+	rejected_ids={}
+	var rejected_list1 = document.getElementById('rejected_list1');
+
+	while (rejected_list1.firstChild) {
+	    if (rejected_list1.firstChild.className = "sample") {
+		if (typeof(rejected_list1.firstChild.id) != 'undefined') {		    
+		    samples[rejected_list1.firstChild.id].score = -100;
+		    rejected_ids[rejected_list1.firstChild.id] = "rejected";
+		}
+	    }
+	    rejected_list1.removeChild(rejected_list1.firstChild);	    
+	}	
+
+	//console.log("Rejected list: ");
+	//console.log(rejected_ids);
+	score_to_give = (4 - Object.keys(rejected_ids).length) /2 ;
+	
+	while (list1.firstChild) {	    
 	    if (list1.firstChild.className = "sample") {
 		if (typeof(list1.firstChild.id) != 'undefined') {
-		    samples[list1.firstChild.id].score += score_to_give;
-		    score_to_give -= 1;
+		    if (list1.firstChild.id in rejected_ids) {
+			dummy=1;
+			//console.log("Marked as garbage: " +  list1.firstChild.id + " " + list1.firstChild.innerHTML)
+		    }
+		    else {
+			//console.log("Awarding score " + score_to_give + " to  "+ list1.firstChild.id + " " + list1.firstChild.innerHTML);
+			//console.log(list1.firstChild)
+			samples[list1.firstChild.id].score += score_to_give;
+			score_to_give -= 1;
+		    }
 		}
 	    }
 	    list1.removeChild(list1.firstChild);	    
 	}
 	   
-	var rejected_list1 = document.getElementById('rejected_list1');
-	while (rejected_list1.firstChild) {
-	    if (rejected_list1.firstChild.className = "sample") {
-		if (typeof(rejected_list1.firstChild.id) != 'undefined') {
-		    samples[rejected_list1.firstChild.id].score = -100;
-		}
-	    }
-	    rejected_list1.removeChild(rejected_list1.firstChild);	    
-	}	
+
 
 
 	updateAllSamplesList();
@@ -39,8 +55,8 @@ var updateOrder = function(samples) {
 	}
 
 	var addToList2 = function(index, list) {
-	    console.log(samples);
-	    console.log(index);
+	    //console.log(samples);
+	    //console.log(index);
 	    sample = samples[index[0]];
 	    //console.log(sample);
 
